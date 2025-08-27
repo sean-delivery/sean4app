2793820c50d7464821c612009b7d0a79077b171d
sean4app <seannon29@gmail.com>
2025-08-27 17:48:05 +0000

    fix: supabase import path working on netlify

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,4 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ['@supabase/supabase-js', 'react-router-dom']
-    }
-  }
 })
