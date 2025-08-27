7a2c7a407697ca6a6bdad06baa3ba388abdf3827
sean4app <seannon29@gmail.com>
2025-08-27 16:46:53 +0000

    update App.jsx with centered apps grid and new design

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,4 +1,4 @@
-import { createClient } from '@supabase/supabase-js'
+import { supabase } from "./supabaseClient";
 
 // המשתנים נמשכים מה־Netlify environment variables
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
